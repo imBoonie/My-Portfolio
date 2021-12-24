c86ef70b30753c7b2174edf00c8c24197d80373b
David Jones <30729273+imBoonie@users.noreply.github.com>
2021-12-24 09:47:04 +0000

    Christmas eve update

--- app.js
+++ app.js
@@ -1,35 +1,47 @@
+let myStr = "Hi, My name is David Jones. An aspiring developer from the UK"
+let typeWriterStrings = ["Hi, my name is", "David Jones", "an aspiring developer", "from the UK"];
 
-let txt = "Hi, I'm David Jones"
-let txt2 = "A developer from the UK" 
 let speed = 85; /* The speed/duration of the effect in milliseconds */
 
 let myArr = [];
 let i = 0;
 let j = 0;
+let k = 0;
+let p = 0;
 let squares = document.getElementsByClassName("squares")
-const texts = ["Hii I'm David Jones", "A developer from the UK"]
 
 
-typeWriter();
 
-setTimeout(() => {
-  sloganTyper();
-}, 1500);
 
-function typeWriter() {
-  if (i < txt.length) {
-    document.getElementById("intro1").innerHTML += txt.charAt(i);
-    i++;
-    setTimeout(typeWriter, speed);
+function myWriter() {
+  for (let i = 0; i < typeWriterStrings.length; i++) {
+   
+    for (let j = 0; j < typeWriterStrings[i].length; ){
+      if ([i] == 0) {
+      document.getElementById("intro1").innerHTML += typeWriterStrings[i].charAt(j);
+      j++
+      } else if ([i] == 1) {
+      document.getElementById("intro2").innerHTML += typeWriterStrings[i].charAt(j);
+      j++
+      } else if ([i] == 2) {
+      document.getElementById("intro3").innerHTML += typeWriterStrings[i].charAt(j);
+      j++
+      } else if ([i] == 3) {
+      document.getElementById("intro4").innerHTML += typeWriterStrings[i].charAt(j); 
+      j++ 
+      }
+    }
   }
 }
 
-function sloganTyper() {
-  if (j < txt2.length)
-  document.getElementById("intro2").innerHTML += txt2.charAt(j);
-  j++;
-  setTimeout(sloganTyper, speed);
-}
+window.addEventListener("load", myInit, true); 
+function myInit(){ 
+ 
+  setTimeout(() => {
+  }, 1700);
+  creator();
+}; 
+
 
 
 function getRandomNum(max, min) {
@@ -37,19 +49,36 @@ function getRandomNum(max, min) {
 }
 
 function creator() {
-  for(i = 0; i < squares.length; i++) {
-    if(i == !squares.length) {
+  for(let k = 0; k < squares.length; k++) {
+    if(k == !squares.length) {
       shapeLoader();
     }
   }
 }
 
 function shapeLoader() {
-  for (j = 0; j <squares.length; j++) {
-    squares[j].style.height = getRandomNum(150,1) + "px"
-    squares[j].style.width = getRandomNum(150,1) + "px"
-    squares[j].style.left = getRandomNum(100,1) + "vw"
-    squares[j].style.animationDuration = getRandomNum(40,15) + "s"
-    squares[j].style.animationDelay = getRandomNum(20,1) + "s" 
+  for (l = 0; l <squares.length; l++) {
+    squares[l].style.height = getRandomNum(150,1) + "px"
+    squares[l].style.width = getRandomNum(150,1) + "px"
+    squares[l].style.left = getRandomNum(100,0) + "vw"
+    squares[l].style.animationDuration = getRandomNum(100,25) + "s"
+    squares[l].style.animationDelay = getRandomNum(50,0) + "s"
+    
+    
   } 
+}
+
+window.onload = function() {
+  document.getElementById('contact-form').addEventListener('submit', function(event) {
+      event.preventDefault();
+      // generate a five digit number for the contact_number variable
+      this.contact_number.value = Math.random() * 100000 | 0;
+      // these IDs from the previous steps
+      emailjs.sendForm('service_uy3plw3', 'template_0ci8fvj', this)
+          .then(function() {
+              console.log('SUCCESS!');
+          }, function(error) {
+              console.log('FAILED...', error);
+          });
+  });
 }
